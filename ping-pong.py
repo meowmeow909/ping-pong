500320db3ab07ab8cbfae9339d7ea37f40043956
meowmeow909 <meow.meow.0.mmm@gmail.com>
2026-04-04 18:28:01 +0700

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,33 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, picture, x, y, speed):
+        super().__init__()
+        self.picture = transform.scale(image.load(picture), (x, y))
+        self.speed = speed
+        self.rect = self.picture.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw_sprite(self):
+        win.blit(self.picture, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < winWidth - 80:
+            self.rect.y += self.speed
+
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < winWidth - 80:
+            self.rect.y += self.speed
+
+
 backColor = (102, 255, 178)
 winHeight = 700
 winWidth = 500
