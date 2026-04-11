bef3ca83401d13b97fac2034afa51ac4fd21e25a
meowmeow909 <meow.meow.0.mmm@gmail.com>
2026-04-11 17:47:52 +0700

    Update ping-pong.py
    отображение и создание спрайтов

--- ping-pong.py
+++ ping-pong.py
@@ -1,13 +1,15 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, picture, x, y, speed):
+    def __init__(self, picture, x, y, width, height, speed):
         super().__init__()
         self.picture = transform.scale(image.load(picture), (x, y))
         self.speed = speed
         self.rect = self.picture.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = width
+        self.height = height
     def draw_sprite(self):
         win.blit(self.picture, (self.rect.x, self.rect.y))
 
@@ -32,16 +34,36 @@ backColor = (102, 255, 178)
 winHeight = 700
 winWidth = 500
 game = True
+finish = False
 FPS = 60
 clock = time.Clock()
+racket = 'racket.png'
+ball = 'ball.png'
 
-win = display.set_mode((winHeigh, winWidth))
+font1 = font.SysFont('Arial', 70)
+winnerRight = font1.render('Right player win!', True, (255, 255, 255))
+winnerLeft = font1.render('Left player win!', True, (255, 255, 255))
+
+win = display.set_mode((winHeight, winWidth))
 win.fill(backColor)
 
 while game:
     for i in event.get():
         if i.type == QUIT:
             game = False
+    #win.fill(backColor)
+
+    racketLeft.update_left()
+    racketRight.update_right()
+
+
+
+
+
+
+    racketLeft.draw_sprite()
+    racketRight.draw_sprite()
+    ball.draw_sprite()
 
 
 
